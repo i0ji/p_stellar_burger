0e57cca96930643eb9b335a395220b87c85c7a5a
i0ji <ioji@inbox.ru>
2024-02-01 13:31:53 +0300

    01.02.24/b:sprint2/step-1/v:0.1.6.2: One-way context pushed to Ingredients Block

--- src/App.tsx
+++ src/App.tsx
@@ -6,60 +6,60 @@ import BurgerConstructor from "components/BurgerConstructor/BurgerConstructor.ts
 import {getIngredients} from "src/utils/burger-api.ts";
 import {IIngredient} from "src/Interfaces";
 
+import {BurgerIngredientsContext} from "services/ingredientsContext.ts";
+
+
 export default function App() {
+	const [state, setState] = useState({
+		ingredientsData: [], error: null
+	});
+	
+	const {ingredientsData, error} = state;
+	
+	{/* ----- GET DATA ON MOUNT ----- */
+	}
+	
+	useEffect(() => {
+		getIngredients()
+			.then(data => setState(prevState => ({...prevState, ingredientsData: data})))
+			.catch(err => {
+				setState(prevState => ({...prevState, error: err.message}));
+				console.error(err);
+			});
+	}, []);
+	
 
-    // const [ingredientsData, setIngredientsData] = useState([]);
-    // const [error, setError] = useState(null);
-
-    const [state, setState] = useState({
-        ingredientsData: [],
-        error: null
-    });
-
-    const {ingredientsData, error} = state;
-
-    useEffect(() => {
-        getIngredients()
-            .then(data => setState(prevState => ({...prevState, ingredientsData: data})))
-            .catch(err => {
-                setState(prevState => ({...prevState, error: err.message}));
-                console.error(err);
-            });
-    }, []);
-
-    function randomData(data: IIngredient[], qty: number) {
-        const innerIngredients = data.filter(elem => elem.type !== 'bun')
-
-        const randomData = [...innerIngredients].sort(() => .5 - Math.random())
-
-        return randomData.slice(0, qty)
-    }
-
-    return (
-        <>
-            {/* ----- APP HEADER -----*/}
-
-            <AppHeader/>
-
-            {/* ----- TWO MAIN BLOCKS -----*/}
-
-            <main className={appStyles.burger_builder}>
-                {
-                    state.error ? (
-                        <p>Произошла ошибка: {error}</p>
-                    ) : (
-                        ingredientsData.length > 0 && (
-                            <>
-                                <BurgerIngredients ingredientsData={ingredientsData}/>
-                                <BurgerConstructor
-                                    ingredientsData={randomData(ingredientsData, 7)}
-                                    fixedData={ingredientsData}
-                                />
-                            </>
-                        )
-                    )
-                }
-            </main>
-        </>
-    )
+	{ /* ----- DATA RANDOMIZER ----- */
+	}
+	
+	function randomData(data: IIngredient[], qty: number) {
+		const innerIngredients = data.filter(elem => elem.type !== 'bun');
+		
+		const randomData = [...innerIngredients].sort(() => .5 - Math.random());
+		
+		return randomData.slice(0, qty);
+	}
+	
+	return (<>
+		{/* ----- APP HEADER -----*/}
+		
+		<AppHeader/>
+		
+		{/* ----- TWO MAIN BLOCKS -----*/}
+		
+		<main className={appStyles.burger_builder}>
+			{state.error ? (<p>Произошла ошибка: {error}</p>) : (ingredientsData.length > 0 && (<>
+				
+				<BurgerIngredientsContext.Provider value={state.ingredientsData}>
+					<BurgerIngredients/>
+					{/*<BurgerIngredients ingredientsData={ingredientsData}/>*/}
+				</BurgerIngredientsContext.Provider>
+				
+				<BurgerConstructor
+					ingredientsData={randomData(ingredientsData, 7)}
+					fixedData={ingredientsData}
+				/>
+			</>))}
+		</main>
+	</>)
 }
--- src/services/ingredientsContext.ts
+++ src/services/ingredientsContext.ts
@@ -1,3 +1,3 @@
 import React from "react";
 
-export const IngredientsContext = React.createContext([]);
+export const BurgerIngredientsContext = React.createContext([]);
