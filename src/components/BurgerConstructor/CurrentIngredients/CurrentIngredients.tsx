f209c992404996fc105a46bf99fb471cf0057d60
ioji <ioji@inobx.ru>
2024-02-11 21:40:17 +0300

    The drag and drop mechanism is implemented similar with CodeSandbox

--- src/components/BurgerConstructor/CurrentIngredients/CurrentIngredients.tsx
+++ src/components/BurgerConstructor/CurrentIngredients/CurrentIngredients.tsx
@@ -4,12 +4,12 @@ import {IIngredient} from "interfaces/interfaces";
 import {useDispatch} from "react-redux";
 import {removeIngredient} from "slices/constructorSlice.ts";
 import {useRef} from "react";
-import {DropTargetMonitor, useDrag, useDrop} from "react-dnd";
+import {useDrag, useDrop} from "react-dnd";
 import {XYCoord} from "react-dnd";
 
 
 export interface CardProps {
-    id: any;
+    id: number;
     text: string;
     index: number;
     moveCard: (dragIndex: number, hoverIndex: number) => void;
@@ -22,9 +22,9 @@ interface DragItem {
 }
 
 
-export default function CurrentIngredients(ingredient: IIngredient) {
+export default function CurrentIngredients(ingredient: IIngredient, moveIngredient) {
 
-    const ref = useRef()
+    const ref = useRef<HTMLDivElement>(null)
 
     const dispatch = useDispatch();
     const handleRemoveIngredient = (id: string) => {
@@ -45,44 +45,27 @@ export default function CurrentIngredients(ingredient: IIngredient) {
             }
             const dragIndex = item.index;
             const hoverIndex = index;
-            // Don't replace items with themselves
             if (dragIndex === hoverIndex) {
                 return;
             }
-            // Determine rectangle on screen
             const hoverBoundingRect = ref.current?.getBoundingClientRect();
 
-            // Get vertical middle
             const hoverMiddleY =
                 (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
 
-            // Determine mouse position
             const clientOffset = monitor.getClientOffset();
 
-            // Get pixels to the top
             const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
 
-            // Only perform the move when the mouse has crossed half of the items height
-            // When dragging downwards, only move when the cursor is below 50%
-            // When dragging upwards, only move when the cursor is above 50%
-
-            // Dragging downwards
             if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
                 return;
             }
-
-            // Dragging upwards
             if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
                 return;
             }
 
-            // Time to actually perform the action
             moveIngredient(dragIndex, hoverIndex);
 
-            // Note: we're mutating the monitor item here!
-            // Generally it's better to avoid mutations,
-            // but it's good here for the sake of performance
-            // to avoid expensive index searches.
             item.index = hoverIndex;
         },
     });
