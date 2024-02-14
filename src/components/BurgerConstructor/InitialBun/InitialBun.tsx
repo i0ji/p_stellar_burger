fd57d0f7ad0a4584bb5679579d1c986417469100
ioji <ioji@inobx.ru>
2024-02-14 22:36:35 +0300

    v:0.1.7.1.2; Initial bun attempts

---
+++ src/components/BurgerConstructor/InitialBun/InitialBun.tsx
@@ -0,0 +1,17 @@
+// import initialBunStyles from 'InitialBun.module.scss';
+import {ConstructorElement} from "@ya.praktikum/react-developer-burger-ui-components";
+import awaitSpinner from "images/common/awaitSpinner.svg"
+import burgerConstructorStyles from "components/BurgerConstructor/BurgerConstructorStyles.module.scss";
+
+export default function InitialBun() {
+    return (
+        <>
+            <ConstructorElement
+
+            />
+       </>
+    )
+}
+
+
+
