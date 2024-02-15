6b72d63409cc14d89beec8d48731c87936342d4e
ioji <ioji@inobx.ru>
2024-02-15 23:26:48 +0300

    15.02.24/b:sprint2/step-2/v:0.1.7.2 Initial bun set; стилизация кода, приведение в порядок

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,184 +1,192 @@
+import {addIngredient, reorderIngredients} from "slices/constructorSlice.ts"
+import {updateIds} from "slices/orderSlice.ts"
+
 import burgerConstructorStyles from "./BurgerConstructorStyles.module.scss";
+import awaitSpinner from "images/common/awaitSpinner.svg"
+import {IIngredient} from "interfaces/interfaces";
+
 import {ConstructorElement, CurrencyIcon, Button} from "@ya.praktikum/react-developer-burger-ui-components";
+import CurrentIngredients from "components/BurgerConstructor/CurrentIngredients/CurrentIngredients.tsx";
+import Modal from "components/common/Modal/Modal.tsx";
+import OrderDetails from "components/common/Modal/OrderDetails/OrderDetails.tsx";
+import Loader from "components/common/Loader/Loader.tsx";
+import WarningMessage from "components/common/WarningMessage/WarningMessage.tsx";
 
 import {useDispatch, useSelector} from "react-redux";
 import {useCallback, useEffect} from "react";
 import {useDrop} from "react-dnd";
 import useModal from "hooks/useModal.ts";
 
-import {addIngredient, reorderIngredients} from "slices/constructorSlice.ts"
-import {updateIds} from "slices/orderSlice.ts"
-
-import CurrentIngredients from "components/BurgerConstructor/CurrentIngredients/CurrentIngredients.tsx";
-import Modal from "components/common/Modal/Modal.tsx";
-import OrderDetails from "components/common/Modal/OrderDetails/OrderDetails.tsx";
-
-import {IIngredient} from "interfaces/interfaces";
-import Loader from "components/common/Loader/Loader.tsx";
-import WarningMessage from "components/common/WarningMessage/WarningMessage.tsx";
-
-
 export default function BurgerConstructor() {
-	
-	const dispatch = useDispatch();
-	
-	
-	// --------------- SETTING STATE ---------------
-	
-	const {addedIngredients, bun} = useSelector((state: {
-		constructorSlice: { addedIngredients: IIngredient[]; bun: IIngredient | null };
-	}) => state.constructorSlice);
-	
-	
-	// --------------- PRELOADER CONSTANTS ---------------
-	const isLoaded = useSelector(state => state.orderSlice.status);
-	const hasError = useSelector(state => state.orderSlice.error);
-	
-	// --------------- CURRENT IDS ---------------
-	
-	const ingredientIDs = useSelector(state => state.constructorSlice.addedIngredients).map((elem: IIngredient) => elem._id);
-	const bunIDs = useSelector(state => state.constructorSlice.bun);
-	
-	if (bunIDs) {
-		ingredientIDs.push(bunIDs._id)
-	}
-	useEffect(() => {
-		dispatch(updateIds(ingredientIDs));
-	}, [dispatch, addedIngredients, ingredientIDs]);
-	
-	const {isVisible, openModal, closeModal} = useModal(ingredientIDs);
-	
-	
-	// --------------- TOTAL AMOUNT  ---------------
-	
-	const totalAmount = useSelector((state) => state.constructorSlice.totalAmount);
-	
-	
-	// --------------- DROP LOGIC ---------------
-	
-	const [, dropIngredients] = useDrop({
-		accept: ['bun', 'ingredient'],
-		drop: (item: IIngredient) => {
-			dispatch(addIngredient(item));
-		}
-	});
-	
-	const moveIngredient = useCallback((dragIndex: number, hoverIndex: number) => {
-		dispatch(reorderIngredients({dragIndex, hoverIndex}));
-	}, [dispatch]);
-	
-	const renderIngredients =
-		(ingredient: IIngredient, index: number) => {
-			return (
-				<CurrentIngredients
-					key={ingredient.id}
-					ingredient={ingredient}
-					index={index}
-					moveIngredient={moveIngredient}
-				/>
-			)
-		}
-	
-	return (
-		<section
-			className={burgerConstructorStyles.constructor_block}
-		>
-			<div
-				className={`${burgerConstructorStyles.constructor_list} mb-10`}
-				ref={dropIngredients}
-			>
-				
-				
-				{/* --------------- TOP BUN --------------- */}
-				
-				<div className={burgerConstructorStyles.constructor_order_item}>
-					{bun && (
-						<ConstructorElement
-							extraClass={`${burgerConstructorStyles.constructor_item_top}`}
-							type="top"
-							isLocked={true}
-							text={`${bun.name} (верх)`}
-							price={bun.price ?? 0}
-							thumbnail={bun.image}
-						/>
-					)}
-				</div>
-				
-				
-				{/* --------------- INNER INGREDIENTS --------------- */}
-				
-				<div
-					className={burgerConstructorStyles.constructor_order}
-					style={{
-						scrollbarWidth: (addedIngredients.length > 3) ? 'inherit' : 'none',
-						width: (addedIngredients.length > 3) ? '100%' : '97%',
-					}}
-				>
-					{addedIngredients.map((ingredient, index) => (
-						renderIngredients(ingredient, index)
-					))}
-				</div>
-				
-				
-				{/* --------------- BOTTOM BUN --------------- */}
-				
-				<div className={burgerConstructorStyles.constructor_order_item}>
-					{bun && (
-						<ConstructorElement
-							extraClass={`${burgerConstructorStyles.constructor_item_bottom}`}
-							type="bottom"
-							isLocked={true}
-							text={`${bun.name} (низ)`}
-							price={bun.price ?? 0}
-							thumbnail={bun.image}
-						/>
-					)}
-				</div>
-				
-				
-				{/* --------------- PRICE --------------- */}
-				
-				<div className={burgerConstructorStyles.price_info}>
-					<h1 className="text text_type_main-large pr-3">{totalAmount}</h1>
-					<CurrencyIcon type="primary"/>
-					<Button
-						disabled={!bun}
-						extraClass="ml-3"
-						size="large"
-						type="primary"
-						htmlType="button"
-						onClick={openModal}
-					>Оформить заказ</Button>
-				</div>
-				
-				
-				{/* --------------- MODAL ENTER --------------- */}
-				
-				{/*{isVisible &&*/}
-				{/*    <>*/}
-				{/*        <Modal onClose={closeModal}>*/}
-				{/*            <OrderDetails/>*/}
-				{/*        </Modal>*/}
-				{/*    </>*/}
-				{/*}*/}
-				{/* -------------- PRELOADER + MODAL --------------- */}
-				
-				
-				{(isLoaded === 'loading') && !hasError &&
+
+    const dispatch = useDispatch();
+
+
+    // --------------- DECLARING STATES/VARS/CONSTANTS  ---------------
+
+    const {addedIngredients, bun} = useSelector((state: {
+        constructorSlice: { addedIngredients: IIngredient[]; bun: IIngredient | null };
+    }) => state.constructorSlice);
+    // --------------- PRELOADER CONSTANTS
+    const isLoaded = useSelector(state => state.orderSlice.status);
+    const hasError = useSelector(state => state.orderSlice.error);
+    // --------------- CURRENT IDS
+    const ingredientIDs = useSelector(state => state.constructorSlice.addedIngredients).map((elem: IIngredient) => elem._id);
+    const bunIDs = useSelector(state => state.constructorSlice.bun);
+    // --------------- MODAL
+    const {isVisible, openModal, closeModal} = useModal(ingredientIDs);
+    // --------------- TOTAL AMOUNT
+    const totalAmount = useSelector(state => state.constructorSlice.totalAmount);
+    // --------------- BUNS STATE
+    const isBun = useSelector(state => state.constructorSlice.bun);
+    console.log(isBun)
+
+
+    // --------------- CURRENT ID ---------------
+
+    if (bunIDs) {
+        ingredientIDs.push(bunIDs._id)
+    }
+    useEffect(() => {
+        dispatch(updateIds(ingredientIDs));
+    }, [dispatch, addedIngredients, ingredientIDs]);
+
+
+    // --------------- DROP LOGIC ---------------
+
+    const [, dropIngredients] = useDrop({
+        accept: ['bun', 'ingredient'],
+        drop: (item: IIngredient) => {
+            dispatch(addIngredient(item));
+        }
+    });
+
+    const moveIngredient = useCallback((dragIndex: number, hoverIndex: number) => {
+        dispatch(reorderIngredients({dragIndex, hoverIndex}));
+    }, [dispatch]);
+
+    const renderIngredients =
+        (ingredient: IIngredient, index: number) => {
+            return (
+                <CurrentIngredients
+                    key={ingredient.id}
+                    ingredient={ingredient}
+                    index={index}
+                    moveIngredient={moveIngredient}
+                />
+            )
+        }
+
+
+    // --------------- INITIAL BUN ---------------
+
+    const InitialBun = ({pos}: { pos: "top" | "bottom" | undefined }) => {
+
+        return (
+            <ConstructorElement
+                extraClass={burgerConstructorStyles.constructor_item_initial_bun}
+                text={'Перетащите сюда булочку'}
+                type={pos}
+                isLocked={true}
+                thumbnail={awaitSpinner}
+                price={0}
+            />
+        )
+    }
+
+
+    return (
+        <section
+            className={burgerConstructorStyles.constructor_block}
+        >
+            <div
+                className={`${burgerConstructorStyles.constructor_list} mb-10`}
+                ref={dropIngredients}
+            >
+
+
+                {/* --------------- TOP BUN --------------- */}
+
+                {!isBun ? <InitialBun pos={"top"}/> :
+                    <div className={burgerConstructorStyles.constructor_order_item}>
+                        {bun && (
+                            <ConstructorElement
+                                extraClass={`${burgerConstructorStyles.constructor_item_top}`}
+                                type="top"
+                                isLocked={true}
+                                text={`${bun.name} (верх)`}
+                                price={bun.price ?? 0}
+                                thumbnail={bun.image}
+                            />
+                        )}
+                    </div>}
+
+
+                {/* --------------- INNER INGREDIENTS --------------- */}
+
+                <div
+                    className={burgerConstructorStyles.constructor_order}
+                    style={{
+                        scrollbarWidth: (addedIngredients.length > 3) ? 'inherit' : 'none',
+                        width: (addedIngredients.length > 3) ? '100%' : '97%',
+                    }}
+                >
+                    {addedIngredients.map((ingredient, index) => (
+                        renderIngredients(ingredient, index)
+                    ))}
+                </div>
+
+
+                {/* --------------- BOTTOM BUN --------------- */}
+
+                {!isBun ? <InitialBun pos={'bottom'}/> :
+                    <div className={burgerConstructorStyles.constructor_order_item}>
+                        {bun && (
+                            <ConstructorElement
+                                extraClass={`${burgerConstructorStyles.constructor_item_bottom}`}
+                                type="bottom"
+                                isLocked={true}
+                                text={`${bun.name} (низ)`}
+                                price={bun.price ?? 0}
+                                thumbnail={bun.image}
+                            />
+                        )}
+                    </div>}
+
+
+                {/* --------------- PRICE --------------- */}
+
+                <div className={burgerConstructorStyles.price_info}>
+                    <h1 className="text text_type_main-large pr-3">{totalAmount}</h1>
+                    <CurrencyIcon type="primary"/>
+                    <Button
+                        disabled={!bun}
+                        extraClass="ml-3"
+                        size="large"
+                        type="primary"
+                        htmlType="button"
+                        onClick={openModal}
+                    >Оформить заказ</Button>
+                </div>
+
+
+                {/* -------------- MODAL + PRELOADER --------------- */}
+
+                {(isLoaded === 'loading') && !hasError &&
                     <Loader/>
-				}
-				
-				{isLoaded === 'failed' && <WarningMessage/>}
-				
-				
-				{(isLoaded === 'succeeded') && isVisible &&
+                }
+
+                {isLoaded === 'failed' && <WarningMessage/>}
+
+
+                {(isLoaded === 'succeeded') && isVisible &&
                     <>
                         <Modal onClose={closeModal}>
                             <OrderDetails/>
                         </Modal>
                     </>
-				}
-			</div>
-		</section>
-	)
+                }
+            </div>
+        </section>
+    )
 }
--- src/components/BurgerIngredients/IngredientGroup/IngredientGroup.tsx
+++ src/components/BurgerIngredients/IngredientGroup/IngredientGroup.tsx
@@ -1,125 +1,126 @@
+import {updateSelectedIngredient, resetSelectedIngredient} from "slices/currentIngredientSlice.ts";
+
+import ingredientGroupStyles from "./IngredientGroupStyles.module.scss";
+import {IIngredient, IIngredientCardProps, IIngredientGroupProps} from "interfaces/interfaces";
+
+import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import IngredientDetails from "components/common/Modal/IngredientDetails/IngredientDetails.tsx";
+import Modal from "components/common/Modal/Modal.tsx";
+
 import {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useDrag} from "react-dnd";
 
-import ingredientGroupStyles from "./IngredientGroupStyles.module.scss";
-import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
-import {IIngredient, IIngredientCardProps, IIngredientGroupProps} from "interfaces/interfaces";
-import IngredientDetails from "components/common/Modal/IngredientDetails/IngredientDetails.tsx";
-import Modal from "components/common/Modal/Modal.tsx";
-
 import {v4 as uuidv4} from 'uuid';
 
-import {updateSelectedIngredient, resetSelectedIngredient} from "slices/currentIngredientSlice.ts";
-
 export default function IngredientGroup({type, ingredients}: IIngredientGroupProps) {
-	
-	const [selectedIngredient, setSelectedIngredient] = useState<IIngredient | null>(null);
-	
-	const dispatch = useDispatch();
-	
-	const addedIngredients = useSelector(state => state.constructorSlice.addedIngredients);
-	const bunIngredients = useSelector(state => state.constructorSlice.bun);
-	
-	
-	// ----------------- INGREDIENT ITEM CARD -----------------
-	
-	const IngredientCard = ({onOpenModal, image, price, name, type, _id}: IIngredientCardProps) => {
-		const [{isDragging}, drag] = useDrag({
-			type: 'ingredient',
-			item: {
-				id: uuidv4(),
-				name,
-				image,
-				price,
-				type,
-				_id
-			},
-			collect: (monitor) => ({
-				isDragging: monitor.isDragging(),
-			}),
-		});
-		
-		
-		// ----------------- INGREDIENTS COUNTER -----------------
-		
-		const ingredientCount = addedIngredients.reduce((count: number, ingredient: IIngredient) => {
-			return ingredient.name === name ? count + 1 : count;
-		}, 0);
-		
-		const isCurrentBun = bunIngredients && bunIngredients.name === name && bunIngredients.price === price;
-		
-		return (
-			<div
-				ref={drag}
-				className={`${ingredientGroupStyles.ingredient_card} ${isDragging ? ingredientGroupStyles.dragging : ''}`}
-				onClick={onOpenModal}
-			>
-				<img src={image} alt={name}/>
-				<p className="text text_type_digits-default">
-					{price}
-					<CurrencyIcon type="primary"/>
-				</p>
-				<p className="text text_type_main-default pt1">{name}</p>
-				{(ingredientCount > 0) ? <span className="text text_type_digits-default">
+
+    const [selectedIngredient, setSelectedIngredient] = useState<IIngredient | null>(null);
+
+    const dispatch = useDispatch();
+
+    const addedIngredients = useSelector(state => state.constructorSlice.addedIngredients);
+    const bunIngredients = useSelector(state => state.constructorSlice.bun);
+
+
+    // ----------------- INGREDIENT ITEM CARD -----------------
+
+    const IngredientCard = ({onOpenModal, image, price, name, type, _id}: IIngredientCardProps) => {
+        const [{isDragging}, drag] = useDrag({
+            type: 'ingredient',
+            item: {
+                id: uuidv4(),
+                name,
+                image,
+                price,
+                type,
+                _id
+            },
+            collect: (monitor) => ({
+                isDragging: monitor.isDragging(),
+            }),
+        });
+
+
+        // ----------------- INGREDIENTS COUNTER -----------------
+
+        const ingredientCount = addedIngredients.reduce((count: number, ingredient: IIngredient) => {
+            return ingredient.name === name ? count + 1 : count;
+        }, 0);
+
+        const isCurrentBun = bunIngredients && bunIngredients.name === name && bunIngredients.price === price;
+
+        return (
+            <div
+                ref={drag}
+                className={`${ingredientGroupStyles.ingredient_card} ${isDragging ? ingredientGroupStyles.dragging : ''}`}
+                onClick={onOpenModal}
+            >
+                <img src={image} alt={name}/>
+                <p className="text text_type_digits-default">
+                    {price}
+                    <CurrencyIcon type="primary"/>
+                </p>
+                <p className="text text_type_main-default pt1">{name}</p>
+                {(ingredientCount > 0) ? <span className="text text_type_digits-default">
 					{ingredientCount}
 				</span> : null}
-				{isCurrentBun && <span className="text text_type_digits-default">
+                {isCurrentBun && <span className="text text_type_digits-default">
 					2
 				</span>
-				}
-			</div>
-		);
-	};
-	
-	// ----------------- INGREDIENT MODAL OPEN/CLOSE LOGIC -----------------
-	
-	const handleOpenModal = (ingredient: IIngredient) => {
-		setSelectedIngredient(ingredient)
-		dispatch(updateSelectedIngredient(ingredient));
-	};
-	
-	const handleCloseModal = () => {
-		dispatch(resetSelectedIngredient());
-		setSelectedIngredient(null);
-	};
-	
-	
-	return (
-		<div className={ingredientGroupStyles.ingredient_list}>
-			<h3 className="text text_type_main-medium pb-6">{type}</h3>
-			
-			{/* --------------- MAPPING INGREDIENTS FOR EACH GROUP --------------- */}
-			
-			{ingredients.map((ingredientItem: IIngredient, i) => (
-					<IngredientCard
-						key={i}
-						{...ingredientItem}
-						onOpenModal={() => handleOpenModal(ingredientItem)}
-					/>
-				)
-			)}
-			
-			
-			{/* --------------- MODAL ENTER --------------- */}
-			
-			{
-				selectedIngredient && (
-					<Modal
-						onClose={handleCloseModal}
-						selectedIngredient={selectedIngredient}
-					>
-						<IngredientDetails
-							onClose={handleCloseModal}
-							image={selectedIngredient.image || ""}
-							name={selectedIngredient.name}
-							proteins={selectedIngredient.proteins || 0}
-							carbohydrates={selectedIngredient.carbohydrates || 0}
-							calories={selectedIngredient.calories || 0}
-							fat={selectedIngredient.fat || 0}
-						/>
-					</Modal>
-				)}
-		</div>
-	);
+                }
+            </div>
+        );
+    };
+
+    // ----------------- INGREDIENT MODAL OPEN/CLOSE LOGIC -----------------
+
+    const handleOpenModal = (ingredient: IIngredient) => {
+        setSelectedIngredient(ingredient)
+        dispatch(updateSelectedIngredient(ingredient));
+    };
+
+    const handleCloseModal = () => {
+        dispatch(resetSelectedIngredient());
+        setSelectedIngredient(null);
+    };
+
+
+    return (
+        <div className={ingredientGroupStyles.ingredient_list}>
+            <h3 className="text text_type_main-medium pb-6">{type}</h3>
+
+            {/* --------------- MAPPING INGREDIENTS FOR EACH GROUP --------------- */}
+
+            {ingredients.map((ingredientItem: IIngredient, i) => (
+                    <IngredientCard
+                        key={i}
+                        {...ingredientItem}
+                        onOpenModal={() => handleOpenModal(ingredientItem)}
+                    />
+                )
+            )}
+
+
+            {/* --------------- MODAL ENTER --------------- */}
+
+            {
+                selectedIngredient && (
+                    <Modal
+                        onClose={handleCloseModal}
+                        selectedIngredient={selectedIngredient}
+                    >
+                        <IngredientDetails
+                            onClose={handleCloseModal}
+                            image={selectedIngredient.image || ""}
+                            name={selectedIngredient.name}
+                            proteins={selectedIngredient.proteins || 0}
+                            carbohydrates={selectedIngredient.carbohydrates || 0}
+                            calories={selectedIngredient.calories || 0}
+                            fat={selectedIngredient.fat || 0}
+                        />
+                    </Modal>
+                )}
+        </div>
+    );
 }
