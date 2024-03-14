a3acc3abf8169c89723bcce87e3b22c11d089945
i0ji <ioji@inbox.ru>
2024-03-14 15:10:28 +0300

    [tag]

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -152,6 +152,7 @@ export default function BurgerConstructor() {
 				</div>
 				
 				
+				
 				{/* --------------- BOTTOM BUN --------------- */}
 				
 				{!isBun ? <InitialBun pos={'bottom'}/> :
