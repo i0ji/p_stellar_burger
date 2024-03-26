af842c869179b4a44802703dfea39887ed14a04d
ioji <ioji@inobx.ru>
2024-03-26 07:17:39 +0300

    style: small style edits;

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -7,7 +7,6 @@ import {Logo} from "@ya.praktikum/react-developer-burger-ui-components";
 
 export default function AppHeader() {
 
-
 	return (
 		<header className={styles.header}>
 			<nav className={styles.nav}>
