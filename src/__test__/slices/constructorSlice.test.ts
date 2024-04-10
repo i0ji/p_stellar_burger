202d51dd0851d240454d30e77f55ae86a731359a
i0ji <ioji@inbox.ru>
2024-04-10 15:46:03 +0300

    test: constructorSlice test ready;

--- src/__test__/slices/constructorSlice.test.ts
+++ src/__test__/slices/constructorSlice.test.ts
@@ -8,11 +8,18 @@ import constructorReducer, {
 
 describe('constructorSlice test', () => {
     it('should add ingredient', () => {
-        const bun = {_id: '123abc123abc', type: 'bun', price: 5};
-        const action = {type: addIngredient.type, payload: bun};
+        const bun = {
+            _id: '123abc123abc',
+            type: 'bun',
+            price: 5
+        };
+        const action = {
+            type: addIngredient.type,
+            payload: bun
+        };
         const state = constructorReducer(initialState, action);
         expect(state.bun).toEqual(bun);
-        expect(state.totalAmount).toEqual(10); // 5 * 2
+        expect(state.totalAmount).toEqual(10);
     });
 
     it('should remove ingredient', () => {
@@ -21,7 +28,10 @@ describe('constructorSlice test', () => {
             addedIngredients: [{_id: '123abc123abc', type: 'main', price: 2}],
             totalAmount: 2,
         };
-        const action = {type: removeIngredient.type, payload: '123abc123abc'};
+        const action = {
+            type: removeIngredient.type,
+            payload: '123abc123abc'
+        };
         const state = constructorReducer(initialStateWithIngredient, action);
         expect(state.addedIngredients).toHaveLength(0);
         expect(state.totalAmount).toEqual(0);
@@ -36,7 +46,10 @@ describe('constructorSlice test', () => {
             ],
             totalAmount: 5,
         };
-        const action = {type: reorderIngredients.type, payload: {dragIndex: 0, hoverIndex: 1}};
+        const action = {
+            type: reorderIngredients.type,
+            payload: {dragIndex: 0, hoverIndex: 1}
+        };
         const state = constructorReducer(initialStateWithIngredients, action);
         expect(state.addedIngredients).toEqual([
             {_id: '456def456def', type: 'bun', price: 3},
