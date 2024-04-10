64f06e9755c145da498280c8265502c5b62728d9
i0ji <ioji@inbox.ru>
2024-04-10 15:39:48 +0300

    test: authSlice test ready;

--- src/__test__/slices/authSlice.test.ts
+++ src/__test__/slices/authSlice.test.ts
@@ -10,28 +10,43 @@ import authReducer, {initialState, setAuthChecked, setUser} from 'slices/authSli
 jest.mock('utils/api.ts');
 
 describe('authSlice test', () => {
-    it('should handle setUser', () => {
-        const user = {id: 1, username: 'testuser'};
-        const action = {type: setUser.type, payload: user};
+    it('should set user', () => {
+        const user = {
+            id: 1,
+            username: 'testUser'
+        };
+        const action = {
+            type: setUser.type,
+            payload: user
+        };
         const state = authReducer(initialState, action);
         expect(state.user).toEqual(user);
         expect(state.isAuth).toBe(true);
     });
 
-    it('should handle setAuthChecked', () => {
-        const action = {type: setAuthChecked.type, payload: true};
+    it('should check auth', () => {
+        const action = {
+            type: setAuthChecked.type,
+            payload: true
+        };
         const state = authReducer(initialState, action);
         expect(state.authChecked).toBe(true);
     });
 
-    it('should handle loginUser.pending', () => {
+    it('should check login user: pending', () => {
         const state = authReducer(initialState, {type: loginUser.pending.type});
         expect(state.status).toEqual('loading');
     });
 
-    it('should handle loginUser.fulfilled', () => {
-        const user = {id: 1, username: 'testuser'};
-        const action = {type: loginUser.fulfilled.type, payload: user};
+    it('should check login user: fulfilled', () => {
+        const user = {
+            id: 1,
+            username: 'testUser'
+        };
+        const action = {
+            type: loginUser.fulfilled.type,
+            payload: user
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('succeeded');
         expect(state.user).toEqual(user);
@@ -39,80 +54,118 @@ describe('authSlice test', () => {
         expect(state.loginError).toBe(false);
     });
 
-    it('should handle loginUser.rejected', () => {
-        const action = {type: loginUser.rejected.type, error: {message: 'Error message'}};
+    it('should check login user: rejected', () => {
+        const action = {
+            type: loginUser.rejected.type,
+            error: {message: 'Error message'}
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('failed');
         expect(state.error).toEqual('Error message');
         expect(state.loginError).toBe(true);
     });
 
-    it('should handle registerUser.pending', () => {
+    it('should check register user: pending', () => {
         const state = authReducer(initialState, {type: registerUser.pending.type});
         expect(state.status).toEqual('loading');
     });
 
-    it('should handle registerUser.fulfilled', () => {
-        const user = {id: 1, username: 'testuser'};
-        const action = {type: registerUser.fulfilled.type, payload: user};
+    it('should check register user: fulfilled', () => {
+        const user = {
+            id: 1,
+            username: 'testUser'
+        };
+        const action = {
+            type: registerUser.fulfilled.type,
+            payload: user
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('succeeded');
         expect(state.user).toEqual(user);
         expect(state.isAuth).toBe(true);
     });
 
-    it('should handle registerUser.rejected', () => {
-        const action = {type: registerUser.rejected.type, error: {message: 'Error message'}};
+    it('should check register user: rejected', () => {
+        const action = {
+            type: registerUser.rejected.type,
+            error: {message: 'Error message'}
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('failed');
         expect(state.error).toEqual('Error message');
     });
 
-    it('should handle logoutUser.pending', () => {
+    it('should check logout user: pending', () => {
         const state = authReducer(initialState, {type: logoutUser.pending.type});
         expect(state.status).toEqual('loading');
     });
 
-    it('should handle logoutUser.fulfilled', () => {
+    it('should check logout user: fulfilled', () => {
         const state = authReducer(initialState, {type: logoutUser.fulfilled.type});
         expect(state.status).toEqual('idle');
         expect(state.isAuth).toBe(false);
         expect(state.user).toBeNull();
     });
 
-    it('should handle logoutUser.rejected', () => {
-        const action = {type: logoutUser.rejected.type, error: {message: 'Error message'}};
+    it('should check logout user: rejected', () => {
+        const action = {
+            type: logoutUser.rejected.type,
+            error: {message: 'Error message'}
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('failed');
         expect(state.error).toEqual('Error message');
         expect(state.isAuth).toBe(false);
     });
 
-    it('should handle getUserData.fulfilled', () => {
-        const userData = {id: 1, email: 'test@example.com'};
-        const action = {type: getUserData.fulfilled.type, payload: userData};
+    it('should get user data: fulfilled', () => {
+        const userData = {
+            id: 1,
+            email: 'test@example.com'
+        };
+        const action = {
+            type: getUserData.fulfilled.type,
+            payload: userData
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('succeeded');
         expect(state.userData).toEqual(userData);
     });
 
-    it('should handle getUserData.rejected', () => {
-        const action = {type: getUserData.rejected.type, error: {message: 'Error message'}};
+    it('should get user data: rejected', () => {
+        const action = {
+            type: getUserData.rejected.type,
+            error: {message: 'Error message'}
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('failed');
         expect(state.error).toEqual('Error message');
     });
 
-    it('should handle updateUserData.fulfilled', () => {
-        const updatedUserData = {id: 1, email: 'updated@example.com'};
-        const action = {type: updateUserData.fulfilled.type, payload: updatedUserData};
+    it('should update user data: pending', () => {
+        const state = authReducer(initialState, {type: updateUserData.pending.type});
+        expect(state.status).toEqual('loading');
+    });
+
+    it('should update user data: fulfilled', () => {
+        const updatedUserData = {
+            id: 1,
+            email: 'testd@test.com'
+        };
+        const action = {
+            type: updateUserData.fulfilled.type,
+            payload: updatedUserData
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('succeeded');
         expect(state.userData).toEqual(updatedUserData);
     });
 
-    it('should handle updateUserData.rejected', () => {
-        const action = {type: updateUserData.rejected.type, error: {message: 'Error message'}};
+    it('should update user data: rejected', () => {
+        const action = {
+            type: updateUserData.rejected.type,
+            error: {message: 'Error message'}
+        };
         const state = authReducer(initialState, action);
         expect(state.status).toEqual('failed');
         expect(state.error).toEqual('Error message');
--- src/__test__/slices/orderSlice.test.ts
+++ src/__test__/slices/orderSlice.test.ts
@@ -1,53 +1,73 @@
-import { createOrder } from "utils/api.ts";
-import orderReducer, { initialState, updateIds, updateOrderNumber, updateCurrentOrder } from 'slices/orderSlice';
+import {createOrder} from "utils/api.ts";
+import orderReducer, {
+    initialState,
+    updateIds,
+    updateOrderNumber,
+    updateCurrentOrder
+} from 'slices/orderSlice';
 
 jest.mock('utils/api.ts');
 
 describe('orderSlice test', () => {
-    it('should handle updateIds', () => {
-        const action = { type: updateIds.type, payload: ['id1', 'id2'] };
+    it('should update IDs', () => {
+        const action = {
+            type: updateIds.type,
+            payload: ['123abc123abc', '456def456def']
+        };
         const state = orderReducer(initialState, action);
-        expect(state.IDs).toEqual(['id1', 'id2']);
+        expect(state.IDs).toEqual(['123abc123abc', '456def456def']);
     });
 
-    it('should handle updateOrderNumber', () => {
-        const action = { type: updateOrderNumber.type, payload: 123 };
+    it('should update order number', () => {
+        const action = {
+            type: updateOrderNumber.type,
+            payload: 123
+        };
         const state = orderReducer(initialState, action);
         expect(state.orderNumber).toEqual(123);
         expect(state.error).toBeNull();
     });
 
-    it('should handle updateCurrentOrder', () => {
+    it('should update current order', () => {
         const orderData = {
             createdAt: '2024-04-10',
-            ingredients: ['ingredient1', 'ingredient2'],
+            ingredients: ['123abc123abc', '456def456def'],
             name: 'Order 1',
             number: 1,
             status: 'pending',
             _id: '123456',
             updatedAt: '2024-04-10',
         };
-        const action = { type: updateCurrentOrder.type, payload: orderData };
+        const action = {
+            type: updateCurrentOrder.type,
+            payload: orderData
+        };
         const state = orderReducer(initialState, action);
         expect(state.currentOrder).toEqual(orderData);
         expect(state.error).toBeNull();
     });
 
-    it('should handle createOrder.pending', () => {
-        const state = orderReducer(initialState, { type: createOrder.pending.type });
+    it('should create order.pending', () => {
+        const state = orderReducer(initialState, {type: createOrder.pending.type});
         expect(state.status).toEqual('loading');
     });
 
-    it('should handle createOrder.fulfilled', () => {
-        const action = { type: createOrder.fulfilled.type, payload: 123 };
+    it('should create order.fulfilled', () => {
+        const action = {
+            type: createOrder.fulfilled.type,
+            payload: 123
+        };
         const state = orderReducer(initialState, action);
         expect(state.status).toEqual('succeeded');
         expect(state.orderNumber).toEqual(123);
         expect(state.error).toBeNull();
     });
 
-    it('should handle createOrder.rejected', () => {
-        const action = { type: createOrder.rejected.type, error: { message: 'Error message' } };
+    it('should create order.rejected', () => {
+        const action = {
+            type: createOrder.rejected.type,
+            error: {message: 'Error message'}
+        };
         const state = orderReducer(initialState, action);
         expect(state.status).toEqual('failed');
         expect(state.error).toEqual('Error message');
